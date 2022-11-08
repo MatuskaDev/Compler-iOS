33db0925af14b6ad7b67bcf136afcc51887e62fe
Lukáš Matuška <lukas.matuska@icloud.com>
2022-11-08 17:14:03 +0100

    Initial Commit

---
+++ Compler iOS/Compler_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Compler_iOSApp.swift
+//  Compler iOS
+//
+//  Created by Lukáš Matuška on 08.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct Compler_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
